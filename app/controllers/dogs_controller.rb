c3b12eb04c97a662f9f54591e237cfa1472eb709
Kevin Hsieh <kshsieh@wework.com>
2015-09-30 12:54:21 -0700

    more styling add show action and some templating

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,8 +1,16 @@
 class DogsController < ApplicationController
+  before_action :load_dog, only: :show
+
   def index
     @dogs = Dog.all
   end
 
   def show
+  end
+
+  private
+
+  def load_dog
+    @dog = Dog.find(params[:id])
   end
 end
